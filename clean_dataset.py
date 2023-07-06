c873c215182ff50bc9c895b1538a9ced29741520
andreanatile <andrea.natile.an@gmail.com>
2023-07-06 22:14:15 +0200

    importing and merging dataset

--- clean_dataset.py
+++ clean_dataset.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-# Read the CSV file
-data = pd.read_csv('data/Levels_Fyi_Salary_Data.csv')
+# Read the income CSV file with ";" separator
+dataset_income = pd.read_csv('data/income.csv', sep=';')
 
-# Create a dataset from the CSV data
-dataset = pd.DataFrame(data)
+# Drop the first column
+dataset_income = dataset_income.iloc[:, 1:]
+dataset_income=dataset_income.drop(columns=['state'])
+
+#Read the Cost of living CSV file
+dataset_Cost_Living=pd.read_csv('data/Cost_of_living_index.csv')
 
 
-# Get the column names as an array
-column_names = dataset.columns.tolist()
+# Extract city names without additional information and cleaning 
+dataset_Cost_Living['City'] = dataset_Cost_Living['City'].str.split(',', n=1).str[0].str.strip()
+dataset_Cost_Living=dataset_Cost_Living.drop(columns=['Rank','Restaurant Price Index','Cost of Living Index','Groceries Index'])
 
-# Print the column names
-print(column_names)
+# Merge datasets based on the 'City' column
+merged_dataset = pd.merge(dataset_income, dataset_Cost_Living, on='City', how='inner')
 
-#print(dataset)
+print(merged_dataset.columns)
