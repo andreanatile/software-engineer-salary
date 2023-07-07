284cdfde11ada516e5f4221aedf2180985059011
andreanatile <andrea.natile.an@gmail.com>
2023-07-07 22:40:53 +0200

    prettier

--- plot.py
+++ plot.py
@@ -19,11 +19,10 @@ dataset_tot["Savings"]=dataset_tot['net_salary'] -dataset_tot['cost_living_per_y
 #calculate taxes 
 dataset_tot['Taxes']=dataset_tot['gross_salary']-dataset_tot['net_salary']
 
-print(dataset_tot.columns)
 #-------------------------------- plot ---------------------------------------------
 
 # Sort the dataset by savings in descending order
-dataset_tot= dataset_tot.sort_values('Savings', ascending=True)
+dataset_tot = dataset_tot.sort_values('Savings', ascending=True)
 
 # Create a figure and axis
 fig, ax = plt.subplots()
@@ -31,23 +30,43 @@ fig, ax = plt.subplots()
 # Calculate the positions of the bars on the x-axis
 x = range(len(dataset_tot))
 
+# Define new colors for the bars
+savings_color = '#6A0572'
+taxes_color = '#DC143C'
+cost_color = '#228B22'
+
 # Plotting the stacked bars
-ax.bar(x,dataset_tot['Savings'], color='purple', label='Savings')
-ax.bar(x, dataset_tot['Taxes'], color='red', label='Taxes',bottom=dataset_tot['Savings'])
-ax.bar(x, dataset_tot['cost_living_per_year'], color='green', label='Cost of living per year', bottom=dataset_tot['Savings']+dataset_tot['Taxes'])
+ax.bar(x, dataset_tot['Savings'], color=savings_color, label='Savings')
+ax.bar(x, dataset_tot['Taxes'], color=taxes_color, label='Taxes', bottom=dataset_tot['Savings'])
+ax.bar(x, dataset_tot['cost_living_per_year'], color=cost_color, label='Cost of living per year',
+       bottom=dataset_tot['Savings'] + dataset_tot['Taxes'])
 
 # Set the x-axis tick positions and labels
 ax.set_xticks(x)
 ax.set_xticklabels(dataset_tot['City'], rotation=45, ha='right')
 
-# Set the y-axis label
-ax.set_ylabel('Amount')
+# Set the y-axis label in dollars
+ax.set_ylabel('Amount ($)', fontsize=12)
+
+# Format the y-axis labels with a decimal point at the thousands position
+ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: format(x, ',.0f')))
+
 
 # Set the chart title
-ax.set_title('Expenses Breakdown by City')
+ax.set_title('Gross Income and Savings Comparison for Software Engineers in Top Paying Cities')
+
+# Remove the background
+ax.set_facecolor('none')
+fig.set_facecolor('none')
 
 # Add a legend
 ax.legend()
+
+# Remove the border around the plot
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
 
 # Adjust the layout to prevent label cropping
 plt.tight_layout()
